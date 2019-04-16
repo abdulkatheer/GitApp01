9faf0748148b5f0d7a647797de8ddf2993520db4
Mohideen Abdul Katheer M <abdulkatheerm@gmail.com>
2019-04-16 17:07:29 +0530

    Student class is created

---
+++ Student.java
@@ -0,0 +1,11 @@
+public class Student{
+	private String name;
+	private String regNo;
+	public static void main(String[] args){
+		System.out.println("Enter name:");
+		Scanner sc = new Scanner(System.in);
+		name = sc.next();
+		System.out.println("Enter Reg No:);
+		regNo = sc.next();
+	}
+}
